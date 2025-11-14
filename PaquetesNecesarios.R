e3f37d10ea203ef79681a0d0aa16e9fba051281c
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-11-14 16:28:33 +0100

    introducción paquetes necesarios

---
+++ PaquetesNecesarios.R
@@ -0,0 +1,11 @@
+# Paquetes necesarios a tener en RStudio.
+
+install.packages("tinytex") # Para instalar LaTex
+tinytex::install_tinytex() # Para instalar LaTex
+
+
+install.packages("DiagrammeR")
+install.packages("DiagrammeRsvg")
+install.packages("rsvg")
+
+install.packages("rmarkdown")
