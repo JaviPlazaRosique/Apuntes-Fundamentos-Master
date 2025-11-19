6e07f5f51c3661ca86e6354683b2b2df2473c403
Javier Plaza Rosique <j.plazarosique@gmail.com>
2025-11-19 01:10:52 +0100

    comienzo tablas sql

--- PaquetesNecesarios.R
+++ PaquetesNecesarios.R
@@ -3,9 +3,16 @@
 install.packages("tinytex") # Para instalar LaTex
 tinytex::install_tinytex() # Para instalar LaTex
 
+tinytex::tlmgr_install("babel-spanish") # Para tener el español
+
 
 install.packages("DiagrammeR")
 install.packages("DiagrammeRsvg")
 install.packages("rsvg")
 
 install.packages("rmarkdown")
+
+install.packages("kableExtra")
+
+install.packages("DBI")
+install.packages("RSQLite")
